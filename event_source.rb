ec3bfad6de12af4c4b979e5fb52d5ee530a138bc
Toan Nguyen <toan@graduate.uwa.edu.au>
2017-03-12 03:32:50 +1100

    Implement Account aggregate with commands

--- event_source.rb
+++ event_source.rb
@@ -1,28 +1,91 @@
 #!/usr/bin/env ruby
+require 'securerandom'
 
-Event = Struct.new(:type, :payload)
-events = [
-  Event.new(:deposit, {amount: 100}),
-  Event.new(:deposit, {amount: 200}),
-  Event.new(:poop, {}),
-  Event.new(:withdraw, {amount: 50}),
-]
+Event = Struct.new(:aggregate_uuid, :type, :payload)
 
-def transaction(state:, event:)
-  case event.type
-  when :deposit
-    amount = event.payload.fetch(:amount)
-    return state + amount
-  when :withdraw
-    amount = event.payload.fetch(:amount)
-    return state - amount
-  else
-    return state
+EVENTS = []
+
+module EventStore
+  extend self
+
+  def sink(events)
+    EVENTS.concat(events)
+  end
+
+  def events_for_aggregate(aggregate_uuid)
+    EVENTS.select { |event| event.aggregate_uuid == aggregate_uuid }
   end
 end
 
-current_balance = events.reduce(0) do |balance, event|
-  transaction(state: balance, event: event)
+module Account
+  OutOfMoneyError = Class.new(StandardError)
+  Aggregate = Struct.new(:uuid, :balance)
+  Command = Struct.new(:amount)
+
+  module Repository
+    def self.load(aggregate_uuid)
+      EventStore.events_for_aggregate(aggregate_uuid)
+        .reduce(Aggregate.new(aggregate_uuid, 0)) do |account, event|
+
+        case event.type
+        when :deposit
+          amount = event.payload.fetch(:amount)
+          new_balance = account.balance + amount
+          Aggregate.new(aggregate_uuid, new_balance)
+        when :withdraw
+          amount = event.payload.fetch(:amount)
+          new_balance = account.balance - amount
+          Aggregate.new(aggregate_uuid, new_balance)
+        else
+          account
+        end
+      end
+    end
+  end
+
+  module CommandHandler
+    extend self
+
+    def deposit_money(account, command)
+      deposit_amount = command.amount
+
+      [Event.new(account.uuid, :deposit, {amount: deposit_amount})]
+    end
+
+    def withdraw_money(account, command)
+      withdraw_amount = command.amount
+
+      if account.balance < withdraw_amount
+        raise OutOfMoneyError
+      end
+
+      [Event.new(account.uuid, :withdraw, {amount: withdraw_amount})]
+    end
+  end
+
+  module API
+    extend self
+
+    def deposit_money(account_uuid, deposit_amount)
+      command = Command.new(deposit_amount)
+      account = Repository.load(account_uuid)
+      events = CommandHandler.deposit_money(account, command)
+      EventStore.sink(events)
+    end
+
+    def withdraw_money(account_uuid, withdraw_amount)
+      command = Command.new(withdraw_amount)
+      account = Repository.load(account_uuid)
+      events = CommandHandler.withdraw_money(account, command)
+      EventStore.sink(events)
+    end
+  end
 end
 
-puts current_balance
+account_uuid = SecureRandom.uuid
+
+Account::API.deposit_money(account_uuid, 100)
+Account::API.withdraw_money(account_uuid, 50)
+Account::API.deposit_money(account_uuid, 25)
+
+puts Account::Repository.load(account_uuid)
